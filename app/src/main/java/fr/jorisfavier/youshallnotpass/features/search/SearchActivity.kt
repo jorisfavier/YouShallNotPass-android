81868242f7e4377874ac5532050875eba21cc87d
joris favier <joris.favier@soprasteria.com>
2019-03-24 21:25:04 +0100

    [search] Designed search page

--- app/src/main/java/fr/jorisfavier/youshallnotpass/features/search/SearchActivity.kt
+++ app/src/main/java/fr/jorisfavier/youshallnotpass/features/search/SearchActivity.kt
@@ -1,14 +1,17 @@
 package fr.jorisfavier.youshallnotpass.features.search
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import fr.jorisfavier.youshallnotpass.R
 
 class SearchActivity: AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+    }
+
+    override fun onStart() {
+        super.onStart()
     }
 }
