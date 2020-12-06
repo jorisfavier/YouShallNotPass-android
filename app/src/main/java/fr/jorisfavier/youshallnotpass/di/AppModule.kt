39c86b2d9409dcafb66ea38efbc4c95b4d818fd2
jorisfavier <joris.favier@pinch.nl>
2020-12-06 17:24:46 +0100

    [dependencies] Update sharedPreferences api

--- app/src/main/java/fr/jorisfavier/youshallnotpass/di/AppModule.kt
+++ app/src/main/java/fr/jorisfavier/youshallnotpass/di/AppModule.kt
@@ -6,7 +6,7 @@ import android.content.ClipboardManager
 import android.content.ContentResolver
 import android.content.Context
 import android.content.SharedPreferences
-import android.preference.PreferenceManager
+import androidx.preference.PreferenceManager
 import androidx.room.Room
 import dagger.Module
 import dagger.Provides
